1bbe792207f6ced521720396aaf0a6cf5bc84fef
KevinSaurel <kevin.saurel@opendeusto.es>
2023-11-19 20:54:43 +0100

    MotoTest

--- ProyectoConcesionario/Test/CocheTest.java
+++ ProyectoConcesionario/Test/CocheTest.java
@@ -15,15 +15,14 @@ public class CocheTest {
     public void testCocheConstructor() throws ParseException {
         Date matriculacion = new Date(System.currentTimeMillis());
 
-        Coche coche = new Coche("Toyota", "Camry", Color.AZUL, "Sedan", 200, 5, 25000, 400, matriculacion, "Gasolina");
+        Coche coche = new Coche("Toyota", "cora", Color.AZUL, "Sedan", 350, 6, 25000, 400, matriculacion, "Gasolina");
 
-        // Verify that the fields are set correctly
-        assertEquals("Toyota", coche.getMarca());
-        assertEquals("Camry", coche.getModelo());
+
+        assertEquals("cora", coche.getModelo());
         assertEquals(Color.AZUL, coche.getColor());
         assertEquals("Sedan", coche.getTipo());
-        assertEquals(200, coche.getPotencia());
-        assertEquals(5, coche.getNumPlazas());
+        assertEquals(350, coche.getPotencia());
+        assertEquals(6, coche.getNumPlazas());
         assertEquals(25000, coche.getPrecio());
         assertEquals(400, coche.getCuota());
         assertEquals(matriculacion, coche.getMatriculacion());
